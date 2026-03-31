a645365753fff6d965fc5863b4b6dc6ade759273
PeterDingVN <dingthanhthe@gmail.com>
2026-03-31 14:56:23 +0700

    delete data_loader.py file and put all classes into data_access.py

--- data/stock_price/data_access.py
+++ data/stock_price/data_access.py
@@ -1,12 +1,90 @@
 from datetime import datetime
-from data.stock_price.data_loader import LoadData
+from vnstock import Vnstock
+from config.settings import DATA_SOURCE, START_DATE, INTERVAL, N_LAGS
 from pathlib import Path
 import pandas as pd
 
 BASE_DIR = Path(__file__).resolve().parents[0]
 
 
+# =======================================================
+# ============== Load Data From API source Class ========
+# =======================================================
+class LoadData:
+
+    """
+        This class is responsible for loading stock price data from API sources. 
+        It relies on lib: vnstock
+        It fetches data based on symbol, start date, interval, and count back (number of candles) parameters.
+
+        Output: Dataframe with stock price data
+
+        Params:
+        - symbol: Stock name (default: 'VCI')
+        - start: Earliest date for data (default: START_DATE from config)
+        - interval: Interval for stock data (min, day, hr) (default: INTERVAL from config)
+        - count_back: Number of candles to fetch (default: N_LAGS + 1 from config)
+    """
+
+    def __init__(self,
+        symbol: str = 'VCI',
+        start: str = START_DATE,
+        interval: str = INTERVAL,
+        count_back: int = N_LAGS+1
+                 ):
+
+        self.symbol = symbol
+        self.interval = interval
+        self.start = start
+        self.candle_nums = count_back
+
+    # Load data for training
+    def fetch_stock_price_data(self, for_train: bool = True):
+
+        # Loop thru sources
+        for src in DATA_SOURCE:
+            try:
+                stock_ = Vnstock().stock(symbol=self.symbol, source=src)
+                if stock_:
+
+                    # Training data -> take everyhthing back till 2015
+                    if for_train:
+                        print(f"Successfully loaded data for training: {self.symbol} from {src}")
+                        data_ = stock_.quote.history(start=self.start, interval=self.interval)
+
+                    # For pred, take up to N_LAGS + 1 latest data
+                    else:
+                        print(f"Successfully loaded data for prediction: {self.symbol} from {src}")
+                        data_ = stock_.quote.history(count_back=self.candle_nums, interval=self.interval)
+                        if len(data_) != self.candle_nums:
+                            dif = self.candle_nums - len(data_)
+                            data_ = stock_.quote.history(count_back=self.candle_nums+dif, interval=self.interval)
+                            data_ = data_[-self.candle_nums:]
+                    return data_
+
+            except ValueError or ModuleNotFoundError:
+                continue
+        return "No data source available. Retry later."
+    
+
+# =======================================================
+# ============== Access Loaded Data Class ===============
+# =======================================================
 class AccessData(LoadData):
+
+    """
+        This class is responsible for accessing the fetched stock price data, either for training or prediction purposes.
+        It is able to access already-crawled data if the data is up-to-date or used for training purpose.
+
+        Output: Dataframe with stock price data for the specified purpose (train, retrain, pred)
+
+        Params:
+            - all params from LoadData class
+            - purpose: The purpose for accessing data (train, retrain, pred)
+
+    """ 
+
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.file_path = BASE_DIR / "data_cache" / f'{self.symbol}.parquet'
@@ -87,8 +165,12 @@ class AccessData(LoadData):
         # ==== Train ====
         return data_ori # return ori data in case purpose = "train"
 
+# =======================================================
+# ================== TEST CASE =========================
+# =======================================================
 
-# -------------- TEST CASE -----------
+# CMD: python -m data.stock_price.data_access
+
 if __name__ == '__main__':
     CTD = AccessData(symbol='MWG')
     data = CTD.access_data(purpose="pred")
