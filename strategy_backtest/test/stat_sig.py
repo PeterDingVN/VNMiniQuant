24000ae4f7e76331419f09348bc8500e1e953465
PeterDingVN <dingthanhthe@gmail.com>
2026-04-08 21:59:01 +0700

    finish quasi-p value test class

---
+++ strategy_backtest/test/stat_sig.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class StatTest:
+
+    """
+        The class is used to do statistical test with quasi_p value for the Strategy
+        Strategy is a bunch of signals created by a trained algorithm (the algorithm is sw else, optimizing on errors
+        such as MSE, RMSE if we are doing ML => after this the ML is used to create signal => this signal is the Strategy
+        => And our stat test here is testing if such Strategy, or more precisely THE PROCESS FROM TRAINING TO GENERATING
+        SIGNALS AS A WHOLE, is NOT good by chance/luck
+    
+    Returns:
+        A quasi p-value
+    """
+    
+    @staticmethod
+    def quasi_pvalue(finance_perf: float, mcpt_perf: list, verbose: bool = True):
+
+        mcpt_perf = np.asarray(mcpt_perf)
+        all_better_case = np.sum(mcpt_perf >= finance_perf)
+
+        # compute p_value to test if the strat is significant
+        quasi_p = (all_better_case + 1) / (len(mcpt_perf) + 1)
+
+        if verbose:
+            return f"Quasi P value for the Strategy: {quasi_p}"
+
+        else:
+            return quasi_p
