79759362a6c7ccb2e94acc0952ad32205f8d65cc
PeterDingVN <dingthanhthe@gmail.com>
2026-04-09 21:25:45 +0700

    update config

--- config/settings.py
+++ config/settings.py
@@ -6,3 +6,10 @@ INTERVAL = '1d'
 
 # Number of lookback periods
 N_LAGS = 11
+
+# Finance Performance Test config
+RISK_FREE_RATE = 0.64 # ---> US Treasury Bond Rate
+TRADE_PERIOD = 252 # ----> total trade periods per year, subtracting holidays, weekends
+
+# Number of permutation for Monte Carlos Simulation
+NUM_OF_PERM = 1000
