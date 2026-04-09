fb388a30bca518892c9943515e1621705f62ce90
PeterDingVN <dingthanhthe@gmail.com>
2026-04-09 17:59:01 +0700

    set blackswan test to coming-soon

---
+++ strategy_backtest/test/blackswan.py
@@ -0,0 +1,10 @@
+
+'''
+    This class is intended to augment FinancialTest in evaluating performance of a strategy
+    in case of unexpected rise and fall of mkt
+
+    Status:
+        coming soon
+'''
+class BlackSwan:
+    pass
