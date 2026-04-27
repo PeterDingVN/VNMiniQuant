a0a78a29224aaa86c8d9347bcce8eb3d8ece3a43
PeterDingVN <dingthanhthe@gmail.com>
2026-04-27 21:26:18 +0700

    update structure of settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,36 +4,32 @@ from dataclasses import dataclass
 # Project root
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
-# Portfolio (example)
-PORTFOLIO = ['CTD', 'AGR', 'PDR', 'NHA', 'DIG', 'CTS']
-# Data Source Params
+# Dt src and Portfolio (example)
 DATA_SOURCE = ["KBS", "VCI", "TCBS"]
+PORTFOLIO = ['CTD', 'AGR', 'PDR', 'NHA', 'DIG', 'CTS']
 
 # Data
-
 START_DATE = '2015-01-01'
 INTERVAL = '1d'
 
-
-
-# Number of lookback periods
+# Number of data needed to generate ONE VALID PREDICTION
 N_LAGS = 11
 
 # Finance Performance Test config
 RISK_FREE_RATE = 0.64 # ---> US Treasury Bond Rate
 TRADE_PERIOD = 252 # ----> total trade periods per year, subtracting holidays, weekends
+COST_RATE = 0.0017 # -> splippage and trans cost: 0.15% plus 0.02% from slippage, no tax accounted
 
 
-# TA indicator 
-# Strategy 1: EMA x MACD
-LONG_EMA = 233
-SHORT_EMA = 55
-SIGNAL = 50
-EMA_START = 0  # Min start for short (fastlen) and long (slowlen) ema should, if set, be 233
-SIGNAL_START= 0 # signal start should, if set, be 50
-
-# Slipage and Fees
-COST_RATE = 0.0017 # -> 0.15% plus 0.02% from slippage, no tax accounted
+# Strategy 1: EMA crossover + MACD
+@dataclass
+class EmaMacdCfg:
+    LONG_EMA = 233
+    SHORT_EMA = 55
+    SIGNAL = 50
+    EMA_START = 0  # Min start for short (fastlen) and long (slowlen) ema should, if set, be 233
+    SIGNAL_START= 0 # signal start should, if set, be 50
+    
 
 # Core System config
 @dataclass
