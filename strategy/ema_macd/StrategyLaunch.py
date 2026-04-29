5fa097baf92ba79a356e1f22c484bc7f6dfffcf8
PeterDingVN <dingthanhthe@gmail.com>
2026-04-29 09:43:13 +0700

    factor cost into log ret

--- strategy/ema_macd/StrategyLaunch.py
+++ strategy/ema_macd/StrategyLaunch.py
@@ -1,7 +1,9 @@
 from .BuildFeature import EmaMacdFeatures
 from .SignalGen import generate_signals
 from config import EmaMacdCfg
+from strategy_backtest import TransactionCost
 import pandas as pd
+import numpy as np
 
 
 class EmaMacdStrategy:
@@ -19,6 +21,15 @@ class EmaMacdStrategy:
 
         # Add signal
         df_signal = generate_signals(df_fe, start_sig=self.cfg.SIGNAL_START)
+
+        # Add transaction cost
+        cost_col = TransactionCost().transaction_cost_arr(df_signal['final_signal'])
+        df_signal['trans_cost'] = cost_col
+
+        # Final output data
+        df_signal['log_return'] = df_signal['log_return'] + df_signal['trans_cost']
+        df_signal['real_return'] = df_signal['log_return'] * abs(df_signal['final_signal'].shift(1))  
+        df_signal['strat_ret'] = np.cumsum(df_signal['real_return'])
 
         return df_signal
         
