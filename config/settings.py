af52e9961437a8be4bbb983cddfeed6c72aa35aa
PeterDingVN <dingthanhthe@gmail.com>
2026-05-19 16:54:10 +0700

    update setting

--- config/settings.py
+++ config/settings.py
@@ -1,38 +1,53 @@
 from pathlib import Path
 from dataclasses import dataclass
 
+# ================================  STRATEGY SETTINGS  ==================================
+# Change this according to your strategy
+"""
+Syntax is:
+
+config = {
+    "para 1": value,
+    ...
+        }
+
+"""
+
+# Strategy 1: EMA crossover + MACD
+@dataclass
+class EmaCfg:
+    config = {
+        'fastperiod':55,
+        'slowperiod': 233
+    }
+
+
+
+
+# ================================  BASE SETTINGS  ======================================
 # Project root
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
+
 # Dt src and Portfolio (example)
 DATA_SOURCE = ["KBS", "VCI", "TCBS"]
-PORTFOLIO = ['CTD', 'AGR', 'PDR', 'NHA', 'DIG', 'CTS']
 
-# Data -> change bar_per_day according to Interval
+
+# Data Timeframe and settings
 START_DATE = '2015-01-01'
 INTERVAL = '1d'
 BAR_PER_DAY = 1
-
-# Number of data needed to generate ONE VALID PREDICTION
-N_LAGS = 11
-
-# Finance Performance Test config
 RISK_FREE_RATE = 0.0 # ---> US Treasury Bond Rate
 TRADE_PERIOD = 252 # ----> total trade periods per year, subtracting holidays, weekends
 
+
+# Choose stock or future depends on Asset Type
 @dataclass
 class AssetType:
     cost_type = {
     'stock':0.0015, # -> splippage and trans cost: 0.15% plus 0.02% from slippage, no tax accounted
     'future': 0.04
     }
-
-
-# Strategy 1: EMA crossover + MACD
-@dataclass
-class DonchianCfg:
-    lookback = 28
-    
 
 # Core System config
 @dataclass
@@ -45,3 +60,6 @@ class SysConfig:
     perm_start_index=28
     perm_end_index=1
     init_capital = 100
+    
+
+
